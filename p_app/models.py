f064c57b6b9876437a6befe0b9d5f36ad5cb6d0b
K1motho <victorkariuki29@gmail.com>
2025-07-11 12:22:25 +0300

    models complete

--- p_app/models.py
+++ p_app/models.py
@@ -9,7 +9,33 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
     
-class saying (models.Model):
+class Saying (models.Model):
     CULTURES = [
-        
+        ('Greek', 'Ancient Greek'),
+        ('Hebrew', 'Hebrew'),
+        ('Latin', 'Latin'),
+        ('West African', 'West African'),
     ]
+
+    TONES = [
+       ('joyful', 'Joyful'),
+        ('cautionary', 'Cautionary'),
+        ('solemn', 'Solemn'),
+        ('reflective', 'Reflective'),
+        ('humorous', 'Humorous'), 
+    ]
+
+    original_text = models.TextField()
+    translation = models.TextField()
+    explanation = models.TextField()
+    culture = models.CharField(max_length=30, choices=CULTURES)
+    tone = models.CharField(max_length=30, choices=TONES)
+    source = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.translation[:50]}..."
+    
+class SavedSAyings(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    saying = models.ForeignKey(Saying, on_delete=models.CASCADE)
+    Saved_at = models.DateField(auto_now_add=True)
